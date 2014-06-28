a26ea9ff0c698d716742d7d202524963010d257d
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2014-06-29 01:18:45 +0200

    Adding TransientShellSettingsManager

---
+++ Services/TransientShellSettingsManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orchard.Environment.Configuration;
+
+namespace Lombiq.OrchardAppHost.Services
+{
+    /// <summary>
+    /// An <see cref="Orchard.Environment.Configuration.IShellSettingsManager"/> implementation that doesn't persist shell settings.
+    /// </summary>
+    public class TransientShellSettingsManager : IShellSettingsManager
+    {
+        private readonly ConcurrentDictionary<string, ShellSettings> _shellSettings = new ConcurrentDictionary<string,ShellSettings>();
+
+
+        public IEnumerable<ShellSettings> LoadSettings()
+        {
+            return _shellSettings.Values;
+        }
+
+        public void SaveSettings(ShellSettings settings)
+        {
+            _shellSettings.AddOrUpdate(settings.Name, settings, (key, existing) => settings);
+        }
+    }
+}
