c05ea78c47eb7f59adc56b4d14e58b1ebab7b072
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2014-08-19 15:01:44 +0200

    Adding ISessionLocator, ITransactionManager implementation for transient hosts to fix exception with background tasks

---
+++ Environment/TransientHost/NullSessionLocator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orchard.Data;
+using Orchard.Environment.Extensions;
+
+namespace Lombiq.OrchardAppHost.Environment.TransientHost
+{
+    /// <summary>
+    /// The default <see cref="ITransactionManager"/> implementation would fail because of transient hosts not using
+    /// database access. Thus we need so e.g. background tasks run.
+    /// </summary>
+    [OrchardFeature("Lombiq.OrchardAppHost.TransientHost")]
+    [OrchardSuppressDependency("Orchard.Data.SessionLocator")]
+    public class NullSessionLocator : ISessionLocator, ITransactionManager
+    {
+        public NHibernate.ISession For(Type entityType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Demand()
+        {
+        }
+
+        public void RequireNew()
+        {
+        }
+
+        public void RequireNew(System.Data.IsolationLevel level)
+        {
+        }
+
+        public void Cancel()
+        {
+        }
+    }
+}
